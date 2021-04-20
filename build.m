5806070096a7cf76059f69bbb32be31cb0a66ee6
Sophia Vinci-Booher <svincibo@Sophias-iMac.attlocal.net>
2021-04-20 16:48:57 -0400

    changed compiled code

--- build.m
+++ build.m
@@ -1,3 +1,5 @@
 addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/nii.func'));
+addpath(genpath('/func'));
 mcc -m -R -nodisplay -d compiled mp2rage_denoiseUNI.m
 exit
--- compile.sh
+++ compile.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-module load matlab/2019a
+#module load matlab/2019a
 
 mkdir -p compiled
 
 log=compiled/commit_ids.txt
 true > $log
-echo "/N/u/brlife/git/jsonlab" >> $log
-(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+#echo "/N/u/brlife/git/jsonlab" >> $log
+#(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+
+echo "/nii_func" >> $log
+(cd /nii_func && git log -1) >> $log
+
+echo "/func" >> $log
+(cd ../func && git log -1) >> $log
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/nii.func'));
+addpath(genpath('/func'));
 mcc -m -R -nodisplay -d compiled mp2rage_denoiseUNI.m
 exit
 END
